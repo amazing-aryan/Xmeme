0f1d5495e4bd7e255bf07f80cf54b94f37c8484b
Aryan Verma <vermaaryan50@gmail.com>
2021-02-15 19:46:49 +0530

    minor changes

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
-sudo systemctl status mongod
 sudo systemctl enable mongod
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -v
-npm -v
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
 
