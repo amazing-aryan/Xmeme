848ae6ed60da5bd9029ba1997f41b92267a91020
Aryan Verma <vermaaryan50@gmail.com>
2021-02-15 20:45:28 +0530

    minor changes

--- Xmeme-backend/index.js
+++ Xmeme-backend/index.js
@@ -81,7 +81,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8081
 app.listen(PORT, () => {
     console.log('Server is running...')
 })
